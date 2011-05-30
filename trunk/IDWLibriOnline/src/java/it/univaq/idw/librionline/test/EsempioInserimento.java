9af512aaa38ea40f4d16d0686597acd1cb0101bc
giacomolm@gmail.com <giacomolm@gmail.com@39064325-a515-fe3b-a697-b33fab803dc5>
2011-05-30 09:54:03 +0000

    Test Inserimento di un libro

---
+++ trunk/IDWLibriOnline/src/java/it/univaq/idw/librionline/test/EsempioInserimento.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.univaq.idw.librionline.test;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import it.univaq.idw.librionline.model.impl.Libro;
+
+/**
+ *
+ * @author giacomolm
+ */
+public class EsempioInserimento {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("IDWLibriOnlinePU");
+        EntityManager manager = factory.createEntityManager();
+        Libro l = new Libro("28", "TwentyEight");
+        l.setEditore("Mondadori");
+        l.setAnnoPubblicazione(new Integer(1928));
+        l.setRecensione("Il libro racconta la storia dal 1928 ad oggi");
+        //l'inserimento della lingua è delicata, per via di un'altra 
+        manager.getTransaction().begin();
+        manager.persist(l);
+        manager.getTransaction().commit();
+    }
+}
