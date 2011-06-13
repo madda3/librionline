ff6cb7abe26e92062ce9c78ea6463b64549af5dc
zilfio88 <zilfio88@39064325-a515-fe3b-a697-b33fab803dc5>
2011-06-13 15:22:37 +0000

    JS

--- IDWLibriOnline/web/javascript/controlli_form.js
+++ IDWLibriOnline/web/javascript/controlli_form.js
@@ -1,5 +1,6 @@
 ///riferimenti agli oggetti controllati
 var login,user,pass;
+var ricerca,titolo;
 
 //funzione di controllo per il campo di input testuale user
 function checkuser() {
@@ -48,8 +49,44 @@ function checkFormLogin() {
 	//in questo modo, se uno dei due ritorna false, la form non verrà sottomessa
 }
 
+//funzione di controllo per il campo di input testuale titolo
+function checktitolo() {
+	if (titolo.value == "") {
+		//attiviamo il campo
+		titolo.focus();
+		//selezioniamo il testo errato
+		titolo.select();
+		//e bordiamo di rosso il campo da correggere
+		titolo.style.border="1px solid red";
+                //finestra che indica l'errore
+                alert("Lunghezza del campo \'Titolo\' vuoto!");
+		//ritorniamo false per indicare che l'azione è fallita
+		return false;		
+	} else {
+		titolo.style.border="";
+		//ritorniamo true per indicare che l'azione è andata a buon fine
+		return true;
+	}
+}
+
+//funzione per il controllo dell'intera form durante la submission
+function checkFormRicerca() {
+	//eseguiamo i controlli dui due campi, e ritorniamo l'AND dei loro risultati
+	return (checktitolo());
+	//in questo modo, se uno dei due ritorna false, la form non verrà sottomessa
+}
+
 window.onload = function() {
-	//acquisiamo i riferimenti alla form e ai camp che volgiamo controllare
+
+        
+        //acquisiamo i riferimenti alla form e ai camp che volgiamo controllare
+        ricerca = document.getElementById('form_ricerca_base');
+	titolo = document.getElementById('titolo');
+	//impostiamo i listener corretti sulla form e sui suoi campi
+	titolo.onchange = function() {return checktitolo();}
+	ricerca.onsubmit = function() {return checkFormRicerca();}
+        
+        //acquisiamo i riferimenti alla form e ai camp che volgiamo controllare
 	login = document.getElementById('form_login');
 	user = document.getElementById('user');
 	pass = document.getElementById('pass');	
