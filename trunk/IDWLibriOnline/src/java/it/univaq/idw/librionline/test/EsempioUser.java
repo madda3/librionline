7abe7dc2bbcf29a729ab719fd87934edc0e4ed13
giacomolm@gmail.com <giacomolm@gmail.com@39064325-a515-fe3b-a697-b33fab803dc5>
2011-06-13 13:39:30 +0000

    Modifica per liste e registrazione con impostazione gruppi

--- trunk/IDWLibriOnline/src/java/it/univaq/idw/librionline/test/EsempioUser.java
+++ trunk/IDWLibriOnline/src/java/it/univaq/idw/librionline/test/EsempioUser.java
@@ -3,6 +3,7 @@
  */
 package it.univaq.idw.librionline.test;
 
+import it.univaq.idw.librionline.model.Gruppo;
 import it.univaq.idw.librionline.model.LibriOnLineDataLayer;
 import it.univaq.idw.librionline.model.User;
 import it.univaq.idw.librionline.model.impl.LibriOnLineDataLayerMysqlImpl;
@@ -15,8 +16,10 @@ public class EsempioUser {
     public static void main(String[] args){
         //Questo è l'oggetto che devi dichiarare per qualsiasi interazione con il DB
         LibriOnLineDataLayer dl = new LibriOnLineDataLayerMysqlImpl();
+        //Ottengo il gruppo che voglio inserire;
+        Gruppo g = dl.getGruppo("registrato"); 
         //Eseguiamo l'inserimento e lo stampiamo
-        System.out.println(dl.insertUser("paperino","paperina", "paperino@pluto.it", "12341234", "Nome", "Cognome", "per123te5g51", "Via Roma", "Paperopoli", "Papera", 12412, null));
+        System.out.println(dl.insertUser("paperino","paperina", "paperino@pluto.it", "12341234", "Nome", "Cognome", "per123te5g51", "Via Roma", "Paperopoli", "Papera", 12412, g));
         //Attenzione! deve esserci una gestione del gruppo, cosa che abbiamo tralasciato
     }
 }
