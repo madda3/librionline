c3533d07ba7a8f83d4924fa5cc124a8ca60ff5c1
zilfio88 <zilfio88@39064325-a515-fe3b-a697-b33fab803dc5>
2011-07-05 15:01:15 +0000

    Zilfio paginazione

---
+++ trunk/IDWLibriOnline/web/javascript/paginazione.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('#page_container_libri').pajinate({
+        items_per_page : 15	
+    });
+});
+
+$(document).ready(function(){
+    $('#page_container_prestiti').pajinate({
+        items_per_page : 15	
+    });
+});
+
+$(document).ready(function(){
+    $('#page_container_storico').pajinate({
+        items_per_page : 15	
+    });
+});
+
+$(document).ready(function(){
+    $('#page_container_schedautenteprestiti').pajinate({
+        items_per_page : 10	
+    });
+});
+
+$(document).ready(function(){
+    $('#page_container_listaprestitiattivi').pajinate({
+        items_per_page : 15	
+    });
+});
+
+$(document).ready(function(){
+    $('#page_container_listaprestitipassivi').pajinate({
+        items_per_page : 15	
+    });
+});
