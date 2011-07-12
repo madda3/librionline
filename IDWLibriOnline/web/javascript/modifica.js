79277642e71ab1eddc1fdbd2f804b9e215d83313
giacomolm@gmail.com <giacomolm@gmail.com@39064325-a515-fe3b-a697-b33fab803dc5>
2011-07-12 08:12:45 +0000

    controlli js

---
+++ IDWLibriOnline/web/javascript/modifica.js
@@ -0,0 +1,37 @@
+function confirm_modifica(){
+    var n = form.length;
+        for(var i = 0; i<n; i++){
+            if(form.elements[i].value == '' && form.elements[i].name != 'notrequired'){
+              alert('Attenzione: il campo '+form.elements[i].name.split("_", 2)[1]+ ' deve essere riempito');
+            //attiviamo il campo
+            form.elements[i].focus();
+            //selezioniamo il testo errato
+            form.elements[i].select();
+            //e bordiamo di rosso il campo da correggere
+            form.elements[i].style.border="1px solid red";
+            return false;
+        }
+    }
+    return true;
+}
+
+
+function confirm_eliminazione(){
+    var richiesta = window.confirm("Procedere con l'eliminazione?");       
+    return richiesta;
+}
+
+window.onload = function() {
+
+        //acquisiamo i riferimenti alla form e ai campi che vogliamo controllare
+        form = document.getElementsByName('update').item(0);
+	elimina = document.getElementById('elimina');
+        
+	//impostiamo i listener corretti sulla form e sui suoi campi
+        if(elimina != null && form != null){
+            form.onsubmit = function() {return confirm_modifica();}
+            elimina.onclick = function() {return confirm_eliminazione();}
+        }
+
+}
+
