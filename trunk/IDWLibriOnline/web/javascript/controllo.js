5c174f759aa7acadd16478e82cafbd22c9140a34
zilfio88 <zilfio88@39064325-a515-fe3b-a697-b33fab803dc5>
2011-07-12 08:30:41 +0000

    Z

--- trunk/IDWLibriOnline/web/javascript/controllo.js
+++ trunk/IDWLibriOnline/web/javascript/controllo.js
@@ -3,6 +3,27 @@ var login,user,pass;
 var ricerca,titolo;
 var regist, userreg, passreg, email, tel, nome, cognome, codicefiscale, indirizzo, citta, provincia, cap;
 
+function confirm_modifica(){
+    var n = form.length;
+        for(var i = 0; i<n; i++){
+            if(form.elements[i].value == '' && form.elements[i].name != 'notrequired'){
+              alert('Attenzione: il campo '+form.elements[i].name.split("_", 2)[1]+ ' deve essere riempito');
+            //attiviamo il campo
+            form.elements[i].focus();
+            //selezioniamo il testo errato
+            form.elements[i].select();
+            //e bordiamo di rosso il campo da correggere
+            form.elements[i].style.border="1px solid red";
+            return false;
+        }
+    }
+    return true;
+}
+
+function confirm_eliminazione(){
+    var richiesta = window.confirm("Procedere con l'eliminazione?");       
+    return richiesta;
+}
 
 function check(elemento,stringa){
     if (elemento.value == "") {
@@ -93,5 +114,15 @@ window.onload = function() {
             provincia.onchange = function() {return check(provincia,"Provincia");}
             cap.onchange = function() {return check(cap,"Cap");}
             regist.onsubmit = function() {return checkFormReg();}
+        }
+
+        //acquisiamo i riferimenti alla form e ai campi che vogliamo controllare
+        form = document.getElementsByName('update').item(0);
+	elimina = document.getElementById('elimina');
+        
+	//impostiamo i listener corretti sulla form e sui suoi campi
+        if(elimina != null && form != null){
+            form.onsubmit = function() {return confirm_modifica();}
+            elimina.onclick = function() {return confirm_eliminazione();}
         }
 }
